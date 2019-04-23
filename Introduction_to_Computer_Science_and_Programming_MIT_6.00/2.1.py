692ed9e82dc2fc80ccb34acb615935aa912523f8
thesis <necmettinceylan@hotmail.com>
2019-04-23 21:51:42 +0300

    progress

---
+++ Introduction_to_Computer_Science_and_Programming_MIT_6.00/2.1.py
@@ -0,0 +1,63 @@
+
+
+# x = 3
+# x= x*x
+# print x
+# n = raw_input('Enter a number: ')
+# print n 
+
+
+# x = 14
+
+# if (x/2)*2 == x:
+	# print 'Even'
+# else: print 'Odd'
+
+
+
+# z = 'b'
+# if 'x'<z:
+	# print 'Hello'
+	# print 'Mom'
+	
+# z = 'b'	
+# if 'x'<z:
+	 # print 'Hello'
+# print 'Mom'
+
+
+# x = 15
+# y = 5
+# z = 2
+# print x,y,z
+
+# if x < y:
+	# if x < z: print 'x is least'
+	# else: print 'z is least'
+# else: print 'y is least'
+
+
+# x = 15
+# y = 5
+# z = 2
+# print x,y,z
+
+# if x<y and x<z: print 'x is least'
+# elif y<z: print 'y is least'
+# else: print 'z is least'
+
+
+y = 0 
+x = 3 
+itersleft = x
+while(itersleft>0):
+	y = y + x
+	itersleft = itersleft - 1
+
+print y 
+
+
+
+
+
+
---
+++ Introduction_to_Computer_Science_and_Programming_MIT_6.00/pset0.py
@@ -0,0 +1,14 @@
+
+# C:\Python25\python.exe .\pset0.py
+
+
+
+
+surname = raw_input('Please enter your surname:')
+
+
+name = raw_input('Please enter your name: ')
+
+
+print name
+print surname
