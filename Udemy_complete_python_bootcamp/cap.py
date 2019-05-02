95a2e5cb93193c700f40eaa8f47c7d230c5ab2ab
thesis <necmettinceylan@hotmail.com>
2019-05-02 18:52:54 +0300

    progress

---
+++ Udemy_complete_python_bootcamp/cap.py
@@ -0,0 +1,14 @@
+def cap_text(text):
+	"""
+	Input a string
+	Output the capitalized string
+	"""
+	return text.capitalize()
+	
+	
+def tit_text(text):
+	"""
+	Input a string
+	Output the titled string
+	"""
+	return text.title()
