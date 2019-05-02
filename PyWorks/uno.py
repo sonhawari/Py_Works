15ed016e13eff6af853648e7fa2b9c3ef7fb8702
thesis <necmettinceylan@hotmail.com>
2019-05-02 19:52:12 +0300

    progress

---
+++ PyWorks/uno.py
@@ -0,0 +1,8 @@
+# Convert empty elements of text to character of "_"
+
+
+s = "Probabilistic Systems Analysis and Applied Probability"
+
+#s = input("Please enter text to change empty lines with _ : ")
+
+s.replace(" ","_")
