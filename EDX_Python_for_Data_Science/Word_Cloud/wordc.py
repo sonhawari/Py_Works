86dce24d0f6db15f6fc6262e930c31f1e71ac585
thesis <necmettinceylan@hotmail.com>
2019-05-03 15:12:40 +0300

    progress

---
+++ EDX_Python_for_Data_Science/Word_Cloud/wordc.py
@@ -0,0 +1,7 @@
+
+
+f = open("98-0.txt","r")
+
+x_file = open(C:\Users\eiree\Documents\GitHub\Py_Works\EDX_Python_for_Data_Science\Word_Cloud+"/98-0.txt","r")
+
+y_file = open(os.path.join(direct, "5_1.txt"), "r")  
--- PyWorks/uno.py
+++
@@ -1,8 +0,0 @@
-# Convert empty elements of text to character of "_"
-
-
-s = "Probabilistic Systems Analysis and Applied Probability"
-
-#s = input("Please enter text to change empty lines with _ : ")
-
-s.replace(" ","_")
