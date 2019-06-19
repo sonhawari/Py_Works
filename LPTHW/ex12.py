28c089c543a834ed21ef9db3825557940c283aa0
thesis <necmettinceylan@hotmail.com>
2019-06-19 12:44:31 +0300

    progress

--- LPTHW/ex12.py
+++
@@ -1,7 +0,0 @@
-
-
-age = raw_input("How old are you? ")
-height = raw_input("How toll are you? ")
-weight = raw_input("How much do you weigh? ")
-
-print "So, ur %r old, %r high, %r heavy" % (age, height, weight)
