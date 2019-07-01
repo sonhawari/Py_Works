7a3a9a1bb2a6cff53f410e8f44a9f3d45879e69d
thesis <necmettinceylan@hotmail.com>
2019-07-01 10:54:48 +0300

    progress

---
+++ 000Udemy_the_complete_sql_bootcamp/q44-CheckConst.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE new_users(
+id serial PRIMARY KEY,
+first_name VARCHAR(50),
+birth_date DATE CHECK(birth_date > '1900-01-01'),
+join_date DATE CHECK(join_date > birth_date),
+salary integer CHECK(salary > 0)
+);
---
+++ 000Udemy_the_complete_sql_bootcamp/q45-createViews.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW customer_info AS 
+
+SELECT first_name, last_name,email,address,phone FROM customer
+JOIN address
+ON customer.address_id = address.address_id;
