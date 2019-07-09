e61583225af4c0d101562ca0fa865e80b388cf50
Neco <necmettinceylan@hotmail.com>
2019-07-09 10:59:40 +0300

    progress

--- Udemy - Machine Learning A-Z™ Hands-On Python & R In Data Science/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.R
+++ Udemy - Machine Learning A-Z™ Hands-On Python & R In Data Science/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.R
@@ -14,3 +14,19 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+
+# -------------------------------------------------------
+# -------------------------------------------------------
+
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
--- Udemy - Machine Learning A-Z™ Hands-On Python & R In Data Science/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Udemy - Machine Learning A-Z™ Hands-On Python & R In Data Science/Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -8,10 +8,10 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -21,3 +21,19 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+
+#--------------------------------------------------------
+
+#--------------------------------------------------------
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
